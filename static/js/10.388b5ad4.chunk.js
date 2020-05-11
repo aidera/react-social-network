fbbfebd04a680df32779a0b176f5f82c534dee77
Evgenia <hope.aidera@gmail.com>
2020-05-11 16:32:49 +0300

    Updates

---
+++ static/js/10.388b5ad4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpway-of-samurai"]=this["webpackJsonpway-of-samurai"]||[]).push([[10],{303:function(t,s,n){t.exports={settings:"Settings_settings__Md2_-"}},306:function(t,s,n){"use strict";n.r(s);var e=n(0),a=n.n(e),i=n(303),r=n.n(i);s.default=function(){return a.a.createElement("div",{className:r.a.settings},"Settings")}}}]);
+//# sourceMappingURL=10.388b5ad4.chunk.js.map
